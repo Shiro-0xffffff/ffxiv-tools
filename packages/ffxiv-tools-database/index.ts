6b4fddbe9a8ae2d60fc6b371f0e0cfad92ed5f15
Shiro <shiro.0xffffff@gmail.com>
2021-03-26 03:32:27 +0800

    chore: 改为 monorepo

---
+++ packages/ffxiv-tools-database/index.ts
@@ -0,0 +1,6 @@
+/**
+ * FF14 工具集 - 数据库
+ */
+export { setCurrentVersion } from './src/version'
+export { Record, Query, Table, loadTable } from './src/table'
+export { DataType } from './src/data-type'
--- src/lib/ffxiv-database/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * FF14 数据库
- */
-export { setCurrentVersion } from './version'
-export { Record, Query, Table, loadTable } from './table'
-export { DataType } from './data-type'
