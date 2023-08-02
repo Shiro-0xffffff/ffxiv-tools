a2ddac0d02eacfe96176783461514b609b7d1c30
Shiro-0xffffff <shiro.0xffffff@gmail.com>
2023-08-03 00:06:29 +0800

    refactor: 回滚逻辑将流视为AsyncIterableIterator而非AsyncIterable，同时也不再可以重复遍历

--- packages/ffxiv-tools-database/src/csv.ts
+++ packages/ffxiv-tools-database/src/csv.ts
@@ -11,68 +11,51 @@ const csvDataFilePath = (version: string, path: string) => (
 )
 
 /**
- * 将可读流转化为可异步遍历的对象
+ * 为可读流创建异步迭代器
  */
-function readableStreamToAsyncIterable<R = any> (stream: ReadableStream<R>): AsyncIterable<R> {
-  return {
-    async * [Symbol.asyncIterator] () {
-      const reader = stream.getReader()
-      try {
-        while (true) {
-          const { done, value } = await reader.read()
-          if (done) return
-          yield value
-        }
-      } finally {
-        reader.releaseLock()
-      }
+async function* getIteratorOfReadableStream<R = any> (stream: ReadableStream<R>): AsyncIterableIterator<R> {
+  const reader = stream.getReader()
+  try {
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) return
+      yield value
     }
+  } finally {
+    reader.releaseLock()
   }
 }
 
 /**
  * 按块读取 CSV 数据文件
  */
-function readCsvDataFile (version: string, path: string): AsyncIterable<string> {
-  return {
-    async * [Symbol.asyncIterator] () {
-      const res = await fetch(csvDataFilePath(version, path))
-      if (!res.body) throw new Error('read data failed')
-      const asyncIterableResBody = readableStreamToAsyncIterable(res.body)
-      const decoder = new TextDecoder()
-      for await (const chunk of asyncIterableResBody) {
-        yield decoder.decode(chunk)
-      }
-    }
+async function* readCsvDataFile (version: string, path: string): AsyncIterableIterator<string> {
+  const res = await fetch(csvDataFilePath(version, path))
+  if (!res.body) throw new Error('read data failed')
+  const bodyIterator = getIteratorOfReadableStream(res.body)
+  const decoder = new TextDecoder()
+  for await (const chunk of bodyIterator) {
+    yield decoder.decode(chunk)
   }
 }
 
 // Node 读本地版本，备用
 // import { createReadStream } from 'fs'
 // import { resolve, join } from 'path'
-// function readCsvDataFile (version: string, path: string): AsyncIterable<string> {
+// async function* readCsvDataFile (version: string, path: string): AsyncIterableIterator<string> {
 //   const dataPath = resolve(__dirname, `../../../../ffxiv-data/${version}/data/${path}`)
-//   return {
-//     [Symbol.asyncIterator] () {
-//       const stream = createReadStream(join(dataPath, version, path), { encoding: 'utf8' })
-//       return stream[Symbol.asyncIterator]()
-//     }
-//   }
+//   const stream = createReadStream(join(dataPath, version, path), { encoding: 'utf8' })
+//   yield* stream[Symbol.asyncIterator]()
 // }
 
 // Node 读远端版本，备用
-// 请求内容暂未缓存
 // import { get } from 'https'
 // import { Readable } from 'stream'
-// function readCsvDataFile (version: string, path: string): AsyncIterable<string> {
-//   return {
-//     async * [Symbol.asyncIterator] () {
-//       const stream = await new Promise<Readable>(resolve => {
-//         get(csvDataFilePath(version, path), res => resolve(res))
-//       })
-//       yield* stream[Symbol.asyncIterator]()
-//     }
-//   }
+// async function* readCsvDataFile (version: string, path: string): AsyncIterableIterator<string> {
+//   const stream = await new Promise<Readable>(resolve => {
+//     get(csvDataFilePath(version, path), res => resolve(res))
+//   })
+//   yield* stream[Symbol.asyncIterator]()
 // }
 
 /**
@@ -94,20 +77,16 @@ function splitStringWithQuotesPreserved (string: string, separator: string): str
 /**
  * 按行读取 CSV 数据文件
  */
-function readCsvDataFileByLine (version: string, path: string, lineSeparator: string = '\n'): AsyncIterable<string> {
-  return {
-    async * [Symbol.asyncIterator] () {
-      const fileContent = await readCsvDataFile(version, path)
-      let buffer = ''
-      for await (const chunk of fileContent) {
-        buffer += chunk
-        const lines = splitStringWithQuotesPreserved(buffer, lineSeparator)
-        for (const line of lines.slice(0, -1)) if (line) yield line
-        buffer = lines[lines.length - 1]
-      }
-      if (buffer) yield buffer
-    }
+async function* readCsvDataFileByLine (version: string, path: string, lineSeparator: string = '\n'): AsyncIterableIterator<string> {
+  const fileContentIterator = readCsvDataFile(version, path)
+  let buffer = ''
+  for await (const chunk of fileContentIterator) {
+    buffer += chunk
+    const lines = splitStringWithQuotesPreserved(buffer, lineSeparator)
+    for (const line of lines.slice(0, -1)) if (line) yield line
+    buffer = lines[lines.length - 1]
   }
+  if (buffer) yield buffer
 }
 
 /**
@@ -138,55 +117,43 @@ export async function loadGameDataFromCsv<N extends keyof GameDataType & string>
 
   // 按行读取 CSV 数据文件的异步迭代器
   const lines = readCsvDataFileByLine(version, `rawexd/${tableName}.csv`)
-  const linesIterator = lines[Symbol.asyncIterator]()
 
   // 读取数据表表头下标行
-  const indicesHeaderLine = await linesIterator.next()
+  const indicesHeaderLine = await lines.next()
   if (indicesHeaderLine.done) throw new Error('indices missing')
   const fields: GameDataField<T>[] = indicesHeaderLine.value.split(',').slice(1).map(() => ({ key: '' as keyof T }))
 
   // 读取数据表表头字段行
-  const keysHeaderLine = await linesIterator.next()
+  const keysHeaderLine = await lines.next()
   if (keysHeaderLine.done) throw new Error('keys missing')
   keysHeaderLine.value.split(',').slice(1).forEach((cellContent, index) => {
     fields[index].key = cellContent as keyof T
   })
 
   // 读取数据表表头类型行
-  const typesHeaderLine = await linesIterator.next()
+  const typesHeaderLine = await lines.next()
   if (typesHeaderLine.done) throw new Error('types missing')
   typesHeaderLine.value.split(',').slice(1).forEach((cellContent, index) => {
     fields[index].type = cellContent
   })
 
-  // 关闭异步迭代器以解除占用，以便后面读取数据行
-  await linesIterator.return!()
-
-  // 用于读取数据行的异步生成器
-  const records: AsyncIterable<GameDataRecord<T>> = {
-    async * [Symbol.asyncIterator] () {
-      // 每次被遍历时重新获取异步迭代器，并跳过前 3 行的元数据行
-      const linesIterator = lines[Symbol.asyncIterator]()
-      for (let lineNumber = 0; lineNumber < 3; lineNumber++) await linesIterator.next()
-
-      // 逐行解析剩下的数据行生成数据
-      const restLines = { [Symbol.asyncIterator]: () => linesIterator }
-      for await (const dataRowContent of restLines) {
-        const dataRowCells = splitStringWithQuotesPreserved(dataRowContent, ',')
-    
-        const id = Number(dataRowCells[0])
-        const fieldsData = dataRowCells.slice(1).map(cellContent => parseCsvCellContent(cellContent) as T[keyof T])
+  // 之后行为数据行，逐行解析并包装为迭代器
+  const records = (async function* (): AsyncIterableIterator<GameDataRecord<T>> {
+    for await (const dataRowContent of lines) {
+      const dataRowCells = splitStringWithQuotesPreserved(dataRowContent, ',')
   
-        const data: T = {}
-        fieldsData.forEach((fieldData, index) => {
-          const key = fields[index].key || `$${index}` as keyof T
-          data[key] = fieldData
-        })
-    
-        yield { id, data }
-      }
+      const id = Number(dataRowCells[0])
+      const fieldsData = dataRowCells.slice(1).map(cellContent => parseCsvCellContent(cellContent) as T[keyof T])
+
+      const data: T = {}
+      fieldsData.forEach((fieldData, index) => {
+        const key = fields[index].key || `$${index}` as keyof T
+        data[key] = fieldData
+      })
+  
+      yield { id, data }
     }
-  }
+  })()
 
   return { fields, records }
 }
